d1b78b2db9d663350d2f789153c9b09382ac8b8d
Sanjeeban <sanjeeban5644@gmail.com>
2023-04-03 16:29:09 +0530

    Made Splash Activity and Card Layouts

---
+++ app/src/main/java/com/example/to_doapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.to_doapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        LottieAnimationView anim = findViewById(R.id.congrats);
+        anim.setAnimation("welcome.json");
+        anim.playAnimation();
+
+
+        Handler hand = new Handler();
+        hand.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+
+
+    }
+}
